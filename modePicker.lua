3c8fc7e028c0b44a1a128fd8c0ed25bdd544b438
Mick McCarthy <mick@mccarthy.tech>
2021-10-08 23:36:00 +0100

    Add logging of key sig

--- modePicker.lua
+++ modePicker.lua
@@ -1,7 +1,15 @@
 local keys = { 'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#/Gb', 'G', 'Ab', 'A', 'Bb', 'B' }
 local modes = { 'Ionian', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Aeolian', 'Locrian' }
+local modeOffsets = { 0, 2, 4, 5, 7, 9, 11 }
 
-local chosenKey = keys[math.random(#keys)]
-local chosenMode = modes[math.random(#modes)]
+local keyIndex = math.random(#keys)
+local modeIndex = math.random(#modes)
 
-print(chosenKey .. ' ' .. chosenMode)
+local initialScaleRootIndex = keyIndex + modeOffsets[modeIndex]
+local scaleRootIndex = initialScaleRootIndex < 13 and initialScaleRootIndex or initialScaleRootIndex - 12
+
+local scaleRoot = keys[scaleRootIndex]
+local scaleMode = modes[modeIndex]
+local key = keys[keyIndex]
+
+print(scaleRoot .. ' ' .. scaleMode .. ' - Key of ' .. key)
